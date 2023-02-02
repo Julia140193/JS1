7166c8261ef31abbbf6cb6ea717c9c509ceb2317
JuliaP <flaeeee@gmail.com>
2023-02-02 21:10:29 +0300

    hw1 commit

---
+++ hw1/HW1script.js
@@ -0,0 +1,11 @@
+// Task 1
+
+let tempC = 28;
+let tempF = (9/5)*tempC + 32;
+console.log(tempF);
+
+// Task 2
+
+let myName = "Юлия"; // почему-то ругается на использование переменной "name", поэтому назвала "myName"
+let admin = myName;
+console.log(admin);
