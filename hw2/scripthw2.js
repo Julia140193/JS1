14b8c854432dda2a315e033ba19928b9aff2f352
JuliaP <flaeeee@gmail.com>
2023-02-03 22:13:04 +0300

    hw2 commit

---
+++ hw2/scripthw2.js
@@ -0,0 +1,41 @@
+// Task 1
+
+// let num1 = Number(prompt("Введите первое число:"));
+// let num2 = Number(prompt("Введите второе число:"));
+
+// if (num1 <= 1 && num2 >= 3) {
+//     console.log("удовлетворяет условию");
+// } else {
+//     console.log("не удовлетворяет условию");
+// }
+
+// Task 2
+
+// let test = true;
+// (test === true) ? console.log('+++') : console.log('---');
+
+// Task 3
+
+
+// let day = Number(prompt("Введите число от 1 до 31:"));
+// if (day >=1 && day <=10) {
+//     console.log("1-ая декада");
+// } else if (day > 10 && day <=20) {
+//     console.log("2-ая декада");
+// } else if (day > 20 && day <=30) {
+//     console.log("3-я декада");
+// } else if (day === 31){
+//     console.log("4-ая декада?");
+// } else console.log("таких дней в месяце не бывает");
+
+// Task 4
+
+// let num = Number(prompt("Введите число:"));
+// let  sot = Math.trunc((num/100)%10);
+// let dec = Math.trunc((num/10)%10);
+// let ed = num % 10;
+// if (num >=100 && num <1000) {
+// console.log(`В числе ${num} количество сотен: ${sot}, десятков: ${dec}, единиц: ${ed}`);
+// } else {
+//     console.log("Введите трехзначное число, например 123");
+// }
