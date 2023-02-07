36b26e1eeae05ae1f79f8f4e6e2b2a0d38fdbff0
JuliaP <flaeeee@gmail.com>
2023-02-07 09:17:24 +0300

    hw3 commit

---
+++ hw3/scripthw3.js
@@ -0,0 +1,53 @@
+// Task 1
+
+// function cube(num) {
+//     let res = num * num * num;
+//     return res;
+// }
+// console.log(cube(2)+cube(3));
+
+//Task 2
+
+// function nalog(salary) {
+//     if (isNaN(salary)) {
+//         console.log(`значение задано неверно`);
+//     } else {
+//         let res = salary * 0.87;
+//         console.log(`Размер заработной платы за вычетом налогов равен ${res}`);
+//     }
+// }
+
+// nalog(Number(prompt(`Введите зарплату без вычета налогов`)));
+
+// Task 3
+
+// function maxNumber (num1, num2, num3) {
+//     let max = (num1> num2 && num1> num3 ? num1 : (num2 > num1 && num2 > num3 ? num2 : num3));
+//     return max;
+// }
+
+// console.log(maxNumber(Number(prompt(`Введите первое число`)), Number(prompt(`Введите второе число`)),Number(prompt(`Введите третье число`))));
+
+// Task 4
+
+
+// const sum = (a,b) => {
+//     return a + b;
+// }
+
+// let diff = (a,b) => {
+//     return diff = (a>b ? a-b : (b>a ? b-a : 0));
+// }
+
+// const mult = (a,b) => {
+//     return a*b;
+// }
+
+// const div = (a,b) => {
+//     return a/b;
+// }
+
+// console.log(sum(2, 6));
+// console.log(diff(2, 6));
+// console.log(mult(2, 6));
+// console.log(div(2, 6));
